c16e3d8e85464700185822734d0863a7137c1c5c
arnaud chevallier <chevallier.a0@gmail.com>
2024-05-24 19:39:12 +1000

    deleted unecessary files

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').config();
 }
 
-const express = require('express')
-const app = express()
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const userRouter = require('./routes/userRouter')
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 //middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/public', express.static(__dirname + '/public'))
-app.use(cookieParser())
-app.use(cors())
+app.use('/public', express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.use(cors());
 
-// Routes
-app.use('/user', userRouter)
-
-
-const port = process.env.PORT
-app.listen(port, () => console.log(`Server listening on port: ${port}`))
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
